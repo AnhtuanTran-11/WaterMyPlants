88c5e806a9772fc10a1fa46edc21fced9253a90e
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-26 15:14:17 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Water my plants TT-WEBFT-7
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
---
+++ src/components/templateComponent/Component.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+// import { startLogin, startSuccess, startFail } from '../../store/actions/loginActions';
+
+import StyledComponent from "./StyledComponent";
+//      change^               change^
+
+const Component = (props) => {
+  console.log(props);
+  //   change^^
+  return (
+    <StyledComponent>
+      {/* change^^ */}
+      test from Component.js
+    </StyledComponent>
+    // change^^
+  );
+};
+
+// const mapStateToProps = (state) => {
+//   return {
+//     userData: state.login.userData,
+//     error: state.login.error,
+//     loadingData: state.login.loadingData
+//   }
+// }
+
+// export default connect(mapStateToProps, {startLogin, startSuccess, startFail})(Component);
+//              change^^
