2401a23ae40342ca93a39efba3d68018b1102155
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-30 11:21:17 -0700

    added to navbar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -8,6 +8,7 @@ const NavBar = (props) => {
       <h1>NavBar</h1>
       {signedIn ? <NavLink to="/profile">Profile</NavLink> : null}
       {signedIn ? <NavLink to="/login">Sign Out</NavLink> : null}
+      {signedIn ? <NavLink to="/myplants">My Plants</NavLink> : null}
     </div>
   );
 };
