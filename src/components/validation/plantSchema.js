57811c332266af57e7808612bc78fa2c39882a0a
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-30 08:59:17 -0700

    userpage and plantValidation

---
+++ src/components/validation/plantSchema.js
@@ -0,0 +1,13 @@
+import * as yup from "yup";
+
+const plantSchema = yup.object().shape({
+  nickname: yup
+    .string()
+    .required("nickname is required")
+    .min(2, "nickname must be a minimum of 8 characters long"),
+  species: yup.string().required("species is required"),
+  h20Frequency: yup.number().required("h20Frequency is required"),
+  // going to figure out how to make it mix of numbers and letters
+});
+
+export default plantSchema;
