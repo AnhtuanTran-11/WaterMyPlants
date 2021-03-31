3582ab8b4fb2dd8894e74aa98259d98e870883c1
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-31 16:27:35 -0700

    watering calculations

--- src/components/myPlants/Plant.jsx
+++ src/components/myPlants/Plant.jsx
@@ -1,19 +1,42 @@
 import React from "react";
-import PlantStyles from "./plantStyling"
+import PlantStyles from "./plantStyling";
 
 const Plant = ({ plant, plantEditor, plantDelete }) => {
+  const waterCalculator = () => {
+    let dayCounter = Math.floor((Date.now() - 1617146130296) / 86400000);
+    let daysToWater = plant.h2oFrequency - (dayCounter % plant.h2oFrequency);
+    if (dayCounter % plant.h2oFrequency === 0) {
+      return "Water Today";
+    } else {
+      return daysToWater;
+    }
+  };
+
+  let waterResult = waterCalculator();
+  console.log(waterResult);
+
   return (
     <PlantStyles>
-    <div className="cardContainer" key={plant.plantId}>
-      <img src="https://bit.ly/3frwdyF" className="plantImg"/>
-      <h2 className="plantNickname">{plant.nickname}</h2>
-      <h3 className="plantSpecies">{plant.species}</h3>
-      <p className="watering">{plant.h2oFrequency}</p>
-      <div className="buttonContainer">
-      <button className="deleteBut" onClick={() => plantDelete(plant.plantId)}> DELETE </button>
-      <button className="editBut" onClick={() => plantEditor(plant)}> EDIT </button>
+      <div className="cardContainer" key={plant.plantId}>
+        <img src="https://bit.ly/3frwdyF" className="plantImg" />
+        <h2 className="plantNickname">{plant.nickname}</h2>
+        <h3 className="plantSpecies">{plant.species}</h3>
+        <p className="watering">{`Water Frequency: every ${plant.h2oFrequency} days`}</p>
+        <p className="watering">{`Days until water: ${waterResult}`}</p>
+        <div className="buttonContainer">
+          <button
+            className="deleteBut"
+            onClick={() => plantDelete(plant.plantId)}
+          >
+            {" "}
+            DELETE{" "}
+          </button>
+          <button className="editBut" onClick={() => plantEditor(plant)}>
+            {" "}
+            EDIT{" "}
+          </button>
+        </div>
       </div>
-    </div>
     </PlantStyles>
   );
 };
