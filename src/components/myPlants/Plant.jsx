c741584f1b08c96d6cb9d36150fcb044dff85d2f
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-03-31 11:40:41 -0700

    styled plant cards

--- src/components/myPlants/Plant.jsx
+++ src/components/myPlants/Plant.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import PlantStyles from "./plantStyling"
 
 const Plant = ({ plant, plantEditor, plantDelete }) => {
   return (
-    <div key={plant.id}>
-      <h1 onClick={() => plantEditor(plant)}>
-        {" "}
-        {plant.nickname} is a {plant.species}{" "}
-        {plant.h2oFrequency &&
-          `that needs to be watered every ${plant.h2oFrequency} days`}
-      </h1>
-      <button onClick={() => plantDelete(plant.plantId)}> DELETE </button>
+    <PlantStyles>
+    <div className="cardContainer" key={plant.plantId}>
+      <img src="https://bit.ly/3frwdyF" className="plantImg"/>
+      <h2 className="plantNickname">{plant.nickname}</h2>
+      <h3 className="plantSpecies">{plant.species}</h3>
+      <p className="watering">{plant.h2oFrequency}</p>
+      <div className="buttonContainer">
+      <button className="deleteBut" onClick={() => plantDelete(plant.plantId)}> DELETE </button>
+      <button className="editBut" onClick={() => plantEditor(plant)}> EDIT </button>
+      </div>
     </div>
+    </PlantStyles>
   );
 };
 
